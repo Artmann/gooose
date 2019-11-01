14343a353aa7b67313ce494da7ceb492b2ebc7fa
Christoffer Artmann <Artgaard@gmail.com>
2019-11-01 14:35:09 +0100

    refactor: Covert New Card Route

--- src/routes/new-card.js
+++ src/routes/new-card.js
@@ -1,43 +1,53 @@
-import React, { useState } from "react";
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import React from 'react';
 
 import NewCardForm from '../components/new-card-form';
 import View from '../styled-components/view';
-import { connect } from 'react-redux';
 
-function newCard({ api, history, match }) {
+const createCardQuery = gql`
+  mutation CreateCard(
+    $boardId: ID!
+    $text: String!
+    $blocked: Boolean
+    $color: String
+    $timebox: Int
+  ) {
+    createCard(boardId: $boardId, text: $text, blocked: $blocked, color: $color, timebox: $timebox) {
+      id
+      blocked
+      color
+      key
+      order
+      timebox
+      text
+    }
+  }
+`;
+
+
+export default function newCard({ history, match }) {
   const boardId = match.params.id;
 
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [ createCard, { error, loading } ] = useMutation(createCardQuery, {
+    onCompleted: (data) => {
+      console.log(data);
 
-  const createCard = async(text, color) => {
-    try {
-      setErrorMessage(null);
-      setIsSubmitting(true);
-
-      await api.createCard(boardId, text, color);
-      
-      setIsSubmitting(false);
       history.push(`/boards/${boardId}`);
-    } catch(error) {
-      setIsSubmitting(false);
-      setErrorMessage(error.toString());
     }
+  });
+
+  const createCardHandler = (text, color) => {
+    createCard({ variables: { boardId, text, color } });
   };
 
   return (
     <View>
-      <NewCardForm 
-        createCard={createCard}
-        errorMessage={errorMessage}
-        isSubmitting={isSubmitting}
+      <NewCardForm
+        createCard={ createCardHandler }
+        errorMessage={ error ? error.message : null }
+        isSubmitting={ loading }
         />
     </View>
   );
 }
-
-const mapStateToProps = ({ data }) => ({
-  boards: data.boards
-});
-
-export default connect(mapStateToProps)(newCard);
